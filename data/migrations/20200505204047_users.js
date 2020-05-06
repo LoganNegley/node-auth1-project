b9017bceae8b08cb37baa4bc5e23c8bf5f0d6aea
Logan Negley <logannegley@gmail.com>
2020-05-05 20:46:05 -0500

    migrations made

--- data/migrations/20200505204047_users.js
+++ data/migrations/20200505204047_users.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
-  
+  return (
+      knex.schema
+      .createTable('user', tbl =>{
+          tbl.increments('id')
+          tbl.string('userName')
+            .unique()
+            .notNullable();
+        tbl.string('password')
+            .notNullable();
+      })
+  )
 };
 
 exports.down = function(knex) {
-  
+  retrun knex.schema
+  .dropTableIfExist('user')
 };
