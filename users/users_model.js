57773edc6b6349a93653ed60e7d6fb81d0092c64
Logan Negley <logannegley@gmail.com>
2020-05-07 18:43:42 -0500

    helper model functions added

---
+++ users/users_model.js
@@ -0,0 +1,24 @@
+const db = require('../data/db.config');
+
+function find(){
+
+};
+
+function findBy(filter){
+
+};
+
+function add(user){
+
+};
+
+function findById(id){
+
+};
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById
+}
