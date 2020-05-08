b34b547475fed7fb9b651361fa564c9f9d79659e
Logan Negley <logannegley@gmail.com>
2020-05-07 21:21:50 -0500

    custom middleware added for session

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
 const Users = require('./users_model');
+const restricted = require('../auth/restriced-middleware');
 
-router.get('/', (req,res) =>{
-    if(req.session && req.session.user){
+router.get('/', restricted, (req,res) =>{
     Users.find()
     .then(users =>{
         res.status(200).json(users)
     })
     .catch(error =>{res.status(500).json({errorMessage:'Error with database'})})
-}else{
-    res.status(401).json({
-        message:'Not logged in'
-    });
-}
 });
 
 module.exports = router
