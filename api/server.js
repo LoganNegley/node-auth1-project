86f1dcea61f68fd7973d506199f3a5383d4ca178
Logan Negley <logannegley@gmail.com>
2020-05-07 20:35:20 -0500

    sessionConfig made

--- api/server.js
+++ api/server.js
@@ -1,13 +1,28 @@
 const express = require ('express');
+const session = require('express-session');
 
 const usersRouter = require('../users/usersRouter');
 const authRouter = require('../auth/auth-router');
 
 const server = express();
 
+const sessionConfig = {
+name:'chocolate-chip',
+secret: 'mysecret',
+cookie:{
+    maxAge: 3600 * 1000,
+    secure: false,
+    httpOnly: true,
+},
+resave: false,
+saveUnitialized: false,
+
+};
+
 server.use(express.json());
 server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) =>{
     res.json({
